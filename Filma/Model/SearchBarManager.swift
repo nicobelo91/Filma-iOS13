9872439661f8406c00f2947d3f9f0c80ea894643
Nico Cobelo <ingcobelo@gmail.com>
2021-01-22 15:57:30 -0300

    Add Readme file

--- Filma/Model/SearchBarManager.swift
+++ Filma/Model/SearchBarManager.swift
@@ -24,22 +24,22 @@ class SearchBarManager: UIViewController {
     }
     
 
-    func configureAlbumSearchController(_ searchController: UISearchController, _ navigationItem: UINavigationItem) {
+    private func configureSearchController(searchController: UISearchController, navigationItem: UINavigationItem, title: String, scopeButtonTitles: [String] = [], selectedScopeButtonIndex: Int = 0) {
         searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Albums"
+        searchController.searchBar.placeholder = title
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         definesPresentationContext = true
+        searchController.searchBar.scopeButtonTitles = scopeButtonTitles
+        searchController.searchBar.selectedScopeButtonIndex = selectedScopeButtonIndex
     }
-    
-    func configurePhotoSearchController(_ searchController: UISearchController, _ navigationItem: UINavigationItem) {
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Photos"
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
-        definesPresentationContext = true
-        searchController.searchBar.scopeButtonTitles = ["1 Column", "2 Columns", "3 Columns"]
-        searchController.searchBar.selectedScopeButtonIndex = 2
 
+
+    func configureAlbumSearchController(_ searchController: UISearchController, _ navigationItem: UINavigationItem) {
+        configureSearchController(searchController: searchController, navigationItem: navigationItem, title: "Search Albums")
+    }
+
+    func configurePhotoSearchController(_ searchController: UISearchController, _ navigationItem: UINavigationItem) {
+        configureSearchController(searchController: searchController, navigationItem: navigationItem, title: "Search Photos", scopeButtonTitles: ["1 Column", "2 Columns", "3 Columns"], selectedScopeButtonIndex: 2)
     }
 }
